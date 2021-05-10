a91117d4d65a0d2f3717e049cec3e1b387806f50
Renzo Scuadroni <renzo.scuadroni@moove-it.com>
2021-05-10 11:04:28 -0300

    Changes

--- react-examples/src/App.js
+++ react-examples/src/App.js
@@ -7,11 +7,11 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Edito <code>src/App.js</code> y se recarga automaticamente
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://github.com/moove-it/bootcamp"
           target="_blank"
           rel="noopener noreferrer"
         >
